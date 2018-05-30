cee0998334d0f66922d85f2eb8e8571b096cfabf
Aung Phyo Pyae Kyaw <aungphyopyaekyaw@gmail.com>
2018-05-30 23:07:08 +0630

    added env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,10 +12,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         //
-        if(env('APP_ENV') !== 'local') { $url->forceSchema('https'); }
+        if(env('APP_ENV') !== 'local') { $url->forceScheme('https'); }
     }
 
     /**
